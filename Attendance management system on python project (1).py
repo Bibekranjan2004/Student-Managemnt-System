d7e0567884616a7fa11360ca34c91b767470d535
Bibek ranjan <144104102+Bibekranjan2004@users.noreply.github.com>
2023-10-31 14:14:30 +0530

    Update Attendance management system on python project (1).py

--- Attendance management system on python project (1).py
+++ Attendance management system on python project (1).py
@@ -1,4 +1,4 @@
-############################################# IMPORTING ################################################
+                                   #### IMPORTING ####
 import tkinter as tk
 from tkinter import ttk
 from tkinter import messagebox as mess
@@ -10,8 +10,7 @@ from PIL import Image
 import pandas as pd
 import datetime
 import time
-
-############################################# FUNCTIONS ################################################
+                                    #### FUNCTIONS ####
 
 def assure_path_exists(path):
     dir = os.path.dirname(path)
